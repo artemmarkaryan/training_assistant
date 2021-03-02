f2fd17a5f346e88f7c22526bcf2bf36c33206a0f
artemmarkaryan <artemandriy@gmail.com>
2021-03-03 02:50:12 +0300

    modified excercise to look better in admin

--- sber_backend/models/exercise.py
+++ sber_backend/models/exercise.py
@@ -2,10 +2,11 @@ from django.db.models import *
 
 
 class Exercise(Model):
+    def __str__(self):
+        return self.name
+
     name = CharField(max_length=128)
     description = TextField()
     muscle_group = ManyToManyField(to='MuscleGroup')
     duration = IntegerField()
     experience = IntegerField()
-
-
