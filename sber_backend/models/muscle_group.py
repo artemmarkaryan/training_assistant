f1d3e7f54aa630cce0c1993719597bfe2480680a
artemmarkaryan <artemandriy@gmail.com>
2021-03-03 14:56:02 +0300

    modified muscle_group admin page

--- sber_backend/models/muscle_group.py
+++ sber_backend/models/muscle_group.py
@@ -3,6 +3,9 @@ from django.db.models import *
 
 # noinspection PyUnresolvedReferences
 class MuscleGroup(Model):
+    def __str__(self):
+        return self.name
+
     name = CharField(max_length=128)
     description = TextField()
 
